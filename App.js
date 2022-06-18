05fd7f0e036341fe22e6bc7c3df95af860b95bde
Felipe <felipe.presidencia@gmail.com>
2022-06-18 18:08:34 -0300

    Versão Github

--- App.js
+++ App.js
@@ -1,31 +1,55 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-function App(){
+function App() {
 
   const [biscoito, setBiscoito] = useState(require('./src/biscoito.png'));
+  const [alterarTexto, setAlterarTexto] = useState('');
+
+  let frases = [  //cria um array em JS
+    'Siga os bons e aprenda com eles.',
+    'O bom-senso vale mais do que muito conhecimento.',
+    'O riso é a menor distância entre duas pessoas.',
+    'Deixe de lado as preocupações e seja feliz.',
+    'Realize o óbvio, pense no improvável e conquiste o impossível.',
+    'Acredite em milagres, mas não dependa deles.',
+    'A maior barreira para o sucesso é o medo do fracasso.'
+  ];
+
+  function quebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * frases.length) //pega um número aleatório
+
+    setAlterarTexto(frases[numeroAleatorio])  // faz o setAlterarTexto receber o numeroAleatorio de frases
+
+    setBiscoito(require('./src/biscoitoAberto.png'));    
+    }
+
+    function reiniciar(){
+      setBiscoito(require('./src/biscoito.png'));
+      setAlterarTexto('');
+  }
 
   return (
-    <View style={styles.container}> 
+    <View style={styles.container}>
 
-        <Image
-          source={biscoito}
-          style={styles.imagem}
-        />     
+      <Image
+        source={biscoito}
+        style={styles.imagem}
+      />
 
-        <Text style={styles.textofrase}>Frase Biscoito</Text>
+      <Text style={styles.textofrase}>{alterarTexto}</Text>
 
-        <TouchableOpacity style={styles.botao} onPress={() => alert('teste')}>
-          <View style={styles.btnArea}>
-            <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
-          </View>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.botao} onPress={quebraBiscoito}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.botao, styles.btnquebrar ]} onPress={() => alert('teste')}>
-          <View style={styles.btnArea}>
-            <Text style={[styles.btnTexto, styles.textoquebrar]}>Quebrar Biscoito</Text>
-          </View>
-        </TouchableOpacity>
+      <TouchableOpacity style={[styles.botao, styles.btnquebrar]} onPress={ reiniciar }>
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, styles.textoquebrar]}>Reiniciar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
 
     </View>
   );
@@ -37,11 +61,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  imagem:{
+  imagem: {
     width: 230,
     height: 230,
   },
-  textofrase:{
+  textofrase: {
     fontSize: 20,
     color: '#dd7b22',
     margin: 30,
@@ -66,7 +90,7 @@ const styles = StyleSheet.create({
     color: '#dd7b22',
   },
   btnquebrar: {
-    marginTop:15, 
+    marginTop: 15,
     borderColor: '#121212'
   },
   textoquebrar: {
