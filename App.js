a21bafe256d78aaba37c5debe8b9d7bd809f2bb6
Felipe <felipe.presidencia@gmail.com>
2022-06-17 23:00:29 -0300

    App.js

--- App.js
+++ App.js
@@ -1,12 +1,77 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 function App(){
-  return(
-    <View>
+
+  const [biscoito, setBiscoito] = useState(require('./src/biscoito.png'));
+
+  return (
+    <View style={styles.container}> 
+
+        <Image
+          source={biscoito}
+          style={styles.imagem}
+        />     
+
+        <Text style={styles.textofrase}>Frase Biscoito</Text>
+
+        <TouchableOpacity style={styles.botao} onPress={() => alert('teste')}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.botao, styles.btnquebrar ]} onPress={() => alert('teste')}>
+          <View style={styles.btnArea}>
+            <Text style={[styles.btnTexto, styles.textoquebrar]}>Quebrar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
 
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imagem:{
+    width: 230,
+    height: 230,
+  },
+  textofrase:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25,
+  },
+  btnArea: {
+    flex: 1,  // faz com que preencha todo o espaço disponível
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22',
+  },
+  btnquebrar: {
+    marginTop:15, 
+    borderColor: '#121212'
+  },
+  textoquebrar: {
+    color: '#121212'
+  },
+})
 
 export default App;
