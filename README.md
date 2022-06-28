e6ef06685d6290829687084b7d6a1b3bf0b38fb6
Felipe <felipe.presidencia@gmail.com>
2022-06-27 23:12:26 -0300

    C1 - App.js, README.md, styles.js

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1 align="center">
-  <img alt="Biscoito da Sorte" height="80" title="Plant Manager" />
+  Biscoito da Sorte
 </h1>
 
 <p align="center">
@@ -10,6 +10,7 @@
 
 
 ![cover](.github/cover.png?style=flat)
+
 
 
 ## 💻 Projeto
---
+++ src/Home/styles.js
@@ -0,0 +1,45 @@
+//vinculado ao App.js
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imagem: {
+    width: 230,
+    height: 230,
+  },
+  textofrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25,
+  },
+  btnArea: {
+    flex: 1,  // faz com que preencha todo o espaço disponível
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22',
+  },
+  btnquebrar: {
+    marginTop: 15,
+    borderColor: '#121212'
+  },
+  textoquebrar: {
+    color: '#121212'
+  },
+})
